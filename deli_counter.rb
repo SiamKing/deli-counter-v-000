19d7d1c94045cc19f455236028a789f0e2d687ac
SiamKing <thesiameseelectric@gmail.com>
2018-10-09 20:11:11 -0700

    I forgot how fun this is

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(line_names)
+    message = 'The line is currently'
+    names = ""
+    if line_names.size > 0
+        line_names.each_with_index do |name, i| 
+            names << "#{i + 1}. #{name} "
+        end
+        puts message + ": " + names.strip
+    else
+        puts message + " empty."
+    end
+end
+
+def take_a_number(deli, name)
+    deli << name
+    puts "Welcome, #{name}. You are number #{deli.size} in line."
+end
+
+def now_serving(line)
+    if line.size == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{line.shift}."
+    end
+end
